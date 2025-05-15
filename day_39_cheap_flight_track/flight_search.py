485a97f4d3fa620b1f43abf596a465ace8e31aff
Oyibo-Efetobor <efetobor3x@gmail.com>
2025-05-15 15:11:55 +0100

    get iata code function working

--- day_39_cheap_flight_track/flight_search.py
+++ day_39_cheap_flight_track/flight_search.py
@@ -1,3 +1,61 @@
-class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+import requests
+import os
+
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# API credentials
+api_key = os.getenv("flight_api_key")
+api_secret = os.getenv("flight_api_secret")
+
+# Step 1: Get the access token
+def get_access_token():
+    security_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+    security_headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    security_parameters = {
+        "grant_type": "client_credentials",
+        "client_id": api_key,
+        "client_secret": api_secret
+    }
+
+    # Make the POST request to get the token
+    security_response = requests.post(
+        url=security_url,
+        headers=security_headers,
+        data=security_parameters
+    )
+    security_response.raise_for_status()  # Raise an error if the request fails
+    security_data = security_response.json()
+    return security_data["access_token"]
+
+bearer_token = get_access_token()
+print(bearer_token)
+
+def get_iata_code():
+    iata_url = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+    
+    iata_headers = {
+        "Authorization" : f"Bearer {bearer_token}"
+    }
+    
+    user_input = input("What city?: ")
+    
+    iata_parameters ={
+        "keyword":user_input
+    }
+    
+    iata_response = requests.get(
+        url=iata_url,
+        params=iata_parameters,
+        headers=iata_headers
+    )
+    
+    iata_response.raise_for_status()
+    iata_data = iata_response.json()
+    return iata_data["data"][0]["iataCode"]
+
+iata = get_iata_code()
+print(iata)
