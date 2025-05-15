e8126ecfc7e09ee0b6c9277a0f0392567bd7af92
Oyibo-Efetobor <efetobor3x@gmail.com>
2025-05-15 16:15:29 +0100

    iata function updated

--- day_39_cheap_flight_track/data_manager.py
+++ day_39_cheap_flight_track/data_manager.py
@@ -1,3 +1,42 @@
-class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+# class DataManager:
+#     #This class is responsible for talking to the Google Sheet.
+#     pass
+from flight_search import FlightSearch
+import requests
+
+flight_search = FlightSearch()
+
+url = "https://api.sheety.co/8144b8d9ce6bc8d13cfe675fa3483490/flightDeals/sheet1"
+row_count = 2
+put_url = f"{url}/"
+
+# Make the GET request
+response = requests.get(url)
+response.raise_for_status()  # Raise an error if the request fails
+
+# Parse the JSON response
+data = response.json()
+
+# Do something with the data
+excel_dict = (data["sheet1"])
+
+
+
+
+
+
+
+for i in range(len(excel_dict)):
+    body = {
+        "sheet1":{
+            "city" : excel_dict[i]["city"],
+            "iataCode": flight_search.get_iata_code(excel_dict[i]["city"])
+        }
+    }
+    
+    put_response = requests.put(
+    url=put_url+f"{excel_dict[i]["id"]}",
+    json=body
+    )
+    
+    put_response.raise_for_status()
--- day_39_cheap_flight_track/flight_search.py
+++ day_39_cheap_flight_track/flight_search.py
@@ -1,61 +1,56 @@
 import requests
 import os
-
 # Load environment variables
 from dotenv import load_dotenv
 load_dotenv()
 
-# API credentials
-api_key = os.getenv("flight_api_key")
-api_secret = os.getenv("flight_api_secret")
+class FlightSearch:
+    def __init__(self):   
 
-# Step 1: Get the access token
-def get_access_token():
-    security_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
-    security_headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    security_parameters = {
-        "grant_type": "client_credentials",
-        "client_id": api_key,
-        "client_secret": api_secret
-    }
+    # API credentials
+        self.api_key = os.getenv("flight_api_key")
+        self.api_secret = os.getenv("flight_api_secret")
 
-    # Make the POST request to get the token
-    security_response = requests.post(
-        url=security_url,
-        headers=security_headers,
-        data=security_parameters
-    )
-    security_response.raise_for_status()  # Raise an error if the request fails
-    security_data = security_response.json()
-    return security_data["access_token"]
+    # Step 1: Get the access token
+    def get_iata_code(self, user_input):
+        security_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+        security_headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        security_parameters = {
+            "grant_type": "client_credentials",
+            "client_id": self.api_key,
+            "client_secret": self.api_secret
+        }
 
-bearer_token = get_access_token()
-print(bearer_token)
+        # Make the POST request to get the token
+        security_response = requests.post(
+            url=security_url,
+            headers=security_headers,
+            data=security_parameters
+        )
+        security_response.raise_for_status()  # Raise an error if the request fails
+        security_data = security_response.json()
+        bearer_token = security_data["access_token"]
 
-def get_iata_code():
-    iata_url = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
-    
-    iata_headers = {
-        "Authorization" : f"Bearer {bearer_token}"
-    }
-    
-    user_input = input("What city?: ")
-    
-    iata_parameters ={
-        "keyword":user_input
-    }
-    
-    iata_response = requests.get(
-        url=iata_url,
-        params=iata_parameters,
-        headers=iata_headers
-    )
-    
-    iata_response.raise_for_status()
-    iata_data = iata_response.json()
-    return iata_data["data"][0]["iataCode"]
+        iata_url = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+        
+        iata_headers = {
+            "Authorization" : f"Bearer {bearer_token}"
+        }
+        
+        
+        iata_parameters ={
+            "keyword":user_input
+        }
+        
+        iata_response = requests.get(
+            url=iata_url,
+            params=iata_parameters,
+            headers=iata_headers
+        )
+        
+        iata_response.raise_for_status()
+        iata_data = iata_response.json()
+        return str(iata_data["data"][0]["iataCode"])
 
-iata = get_iata_code()
-print(iata)
